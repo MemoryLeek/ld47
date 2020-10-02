86eb92bc7f883738eec8d9487c7ed37a278abaef
ostwilkens <carl@ostwilkens.se>
2020-10-02 20:47:28 +0200

    Add Position and Velocity components

---
+++ src/components/Position.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <SFML/System.hpp>
+
+struct Position
+{
+	sf::Vector2f position;
+};
---
+++ src/components/Velocity.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <SFML/System.hpp>
+
+struct Velocity
+{
+	sf::Vector2f velocity;
+};
