7c2fb9f4625a13ba890cfaf086e5586e73de6f99
ostwilkens <carl@ostwilkens.se>
2020-10-03 22:51:29 +0200

    Add kitten script!

--- scripts/Kitten.gd
+++ scripts/Kitten.gd
@@ -1,10 +1,9 @@
-extends Node
+extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var RAND = RandomNumberGenerator.new()
+const SPEED = 30
+var FACING = "Left"
+var ACTION = "Idle"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +11,24 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if ACTION == "Walk":
+		match FACING:
+			"Left": position.x -= SPEED * delta
+			"Right": position.x += SPEED * delta
+			"Up": position.y -= SPEED * delta
+			"Down": position.y += SPEED * delta
+
+	$AnimatedSprite.play(ACTION + "_" + FACING)
+
+
+func _on_Timer_timeout():
+	var i = RAND.randi_range(1, 6)
+
+	match i:
+		1: ACTION = "Walk"
+		2: ACTION = "Idle"
+		3: FACING = "Left"
+		4: FACING = "Right"
+		5: FACING = "Up"
+		6: FACING = "Down"
