2ad4ccad65b04bcb90394e3dc4e41bc4e5acfbb1
Jimmy Bergström <thekwarf@gmail.com>
2020-10-03 11:48:32 +0200

    I walk on WASD

--- src/systems/InputSystem.cpp
+++ src/systems/InputSystem.cpp
@@ -12,19 +12,19 @@ void InputSystem::run(flecs::iter& it, flecs::column<PlayerInput> p)
 	sf::Vector2i direction;
 
 	// TODO - handle input mapping, or at least make it better
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 	{
 		direction.x -= 1;
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
 		direction.x += 1;
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
 		direction.y -= 1;
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 	{
 		direction.y += 1;
 	}
