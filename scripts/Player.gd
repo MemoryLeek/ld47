08fce269f7dbfb16d0e3a9368735cda0f93be2f1
ostwilkens <carl@ostwilkens.se>
2020-10-03 23:45:20 +0200

    Player velocity update

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,15 +4,19 @@ const SPEED = 100
 var FACING = "Down"
 var ACTION = "Idle"
 var attacking = false
+var velocity = Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func mix(a, b, w):
+	return a * w + b * (1 - w)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	var velocity = Vector2(0, 0)
+	var desiredVelocity = Vector2(0, 0)
 	
 	if Input.is_action_pressed("attack"):
 		ACTION = "Attack"
@@ -20,22 +24,23 @@ func _process(delta):
 	elif not attacking:
 		ACTION = "Idle"
 		if Input.is_action_pressed("ui_left"):
-			velocity.x -= SPEED
+			desiredVelocity.x -= SPEED
 			FACING = "Left"
 			ACTION = "Walk"
 		if Input.is_action_pressed("ui_right"):
-			velocity.x += SPEED
+			desiredVelocity.x += SPEED
 			FACING = "Right"
 			ACTION = "Walk"
 		if Input.is_action_pressed("ui_up"):
-			velocity.y -= SPEED
+			desiredVelocity.y -= SPEED
 			FACING = "Up"
 			ACTION = "Walk"
 		if Input.is_action_pressed("ui_down"):
-			velocity.y += SPEED
+			desiredVelocity.y += SPEED
 			FACING = "Down"
 			ACTION = "Walk"
 	
+	velocity = mix(desiredVelocity, velocity, delta * 20)
 	move_and_slide(velocity)
 	
 	$AnimatedSprite.play(ACTION + "_" + FACING)
