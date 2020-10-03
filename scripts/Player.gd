d7e235f743485d7390f0b97a9ee7d2ecc7866ee6
Jimmy Bergström <thekwarf@gmail.com>
2020-10-03 21:58:39 +0200

    You may walk

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
+const SPEED = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_pressed("ui_left"):
+		position.x -= SPEED * delta
+	if Input.is_action_pressed("ui_right"):
+		position.x += SPEED * delta
+	if Input.is_action_pressed("ui_up"):
+		position.y -= SPEED * delta
+	if Input.is_action_pressed("ui_down"):
+		position.y += SPEED * delta
