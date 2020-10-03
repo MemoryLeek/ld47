1db5df1902d7629d790bf04efc962b8da1ce5c60
ostwilkens <carl@ostwilkens.se>
2020-10-03 23:12:36 +0200

    Charging kitten

--- scripts/Kitten.gd
+++ scripts/Kitten.gd
@@ -1,28 +1,52 @@
 extends Node2D
 
 var RAND = RandomNumberGenerator.new()
-const SPEED = 30
 var FACING = "Left"
 var ACTION = "Idle"
+var player
+var charging = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	player = get_tree().current_scene.find_node("Player")
 	pass # Replace with function body.
 
+func calcSpeed():
+	if charging:
+		return 200
+	else:
+		return 30
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#if ACTION == "Idle":
+	var xDist = abs(position.x - player.position.x)
+	var yDist = abs(position.y - player.position.y)
+	
+	if FACING == "Right" and player.position.x > position.x and yDist < 30:
+		charging = true
+		ACTION = "Walk"
+		$ChargingTimer.start(0.5)
+	elif FACING == "Left" and player.position.x < position.x and yDist < 30:
+		charging = true
+		ACTION = "Walk"
+		$ChargingTimer.start(0.5)
+	
 	if ACTION == "Walk":
+		var speed = calcSpeed()
 		match FACING:
-			"Left": position.x -= SPEED * delta
-			"Right": position.x += SPEED * delta
-			"Up": position.y -= SPEED * delta
-			"Down": position.y += SPEED * delta
-
+			"Left": position.x -= speed * delta
+			"Right": position.x += speed * delta
+			"Up": position.y -= speed * delta
+			"Down": position.y += speed * delta
+	
 	$AnimatedSprite.play(ACTION + "_" + FACING)
 
 
 func _on_Timer_timeout():
+	if charging:
+		pass
+	
 	var i = RAND.randi_range(1, 6)
 
 	match i:
@@ -32,3 +56,7 @@ func _on_Timer_timeout():
 		4: FACING = "Right"
 		5: FACING = "Up"
 		6: FACING = "Down"
+
+
+func _on_ChargingTimer_timeout():
+	charging = false
