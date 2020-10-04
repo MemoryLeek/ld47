15a8cc4d3eb7e541e18063009232ad82f5460fdf
Jimmy Bergström <thekwarf@gmail.com>
2020-10-04 16:50:07 +0200

    Chest animation

--- scripts/Chest.gd
+++ scripts/Chest.gd
@@ -6,5 +6,7 @@ onready var _ui : UserInterface = get_node("/root/UI")
 
 func interact():
 	if _has_content:
+		$Sprite_Closed.visible = false
+		$Sprite_Open.visible = true
 		_ui.give_item(content)
 		_has_content = false
